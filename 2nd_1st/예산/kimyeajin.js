6e130ac73a9b04ead066013dfcc3f61055669abc
kimyeajin216099 <86289318+kimyeajin216099@users.noreply.github.com>
2021-07-11 21:17:17 +0900

    Create kimyeajin.js

---
+++ 2nd_1st/예산/kimyeajin.js
@@ -0,0 +1,13 @@
+function solution(d, budget) {
+    var sum=0;
+    var i;
+    
+    d.sort((a,b) => a - b);
+    
+    for(i=0;i<d.length;i++){
+        sum+=d[i];
+        if(sum>budget)break;
+    }
+    
+    return i;
+}
