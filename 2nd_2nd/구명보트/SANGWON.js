203fbc6b2d863bed24fe219fc0266889f6241d92
SangwonKang <72087183+ksone02@users.noreply.github.com>
2021-07-15 22:21:29 +0900

    Update SANGWON.js

--- 2nd_2nd/구명보트/SANGWON.js
+++ 2nd_2nd/구명보트/SANGWON.js
@@ -1,15 +1,14 @@
 function solution(people, limit) {
     var answer = 0;
-    var newArr = people.sort((a,b) => b-a);
+    people.sort((a,b) => a-b); 
     
-    for(var i = 0, j = newArr.length - 1; i < j; i++) {
-        if(newArr[i] + newArr[j] <= limit) {
-            j--;
+    var j = 0;
+    for(var i = people.length - 1; j <= i; i--) {
+        if(people[i] + people[j] <= limit) { 
+            j++;
         } 
         answer++;
-        
     }
     
     return answer;
 }
-//미완
