41b6826c4d0747058359e28015e2698af2ebadb5
kimyeajin216099 <86289318+kimyeajin216099@users.noreply.github.com>
2021-07-16 10:03:57 +0900

    Update kimyeajin.js

--- 2nd_2nd/구명보트/kimyeajin.js
+++ 2nd_2nd/구명보트/kimyeajin.js
@@ -53,3 +53,24 @@ function solution(people, limit) {
     
     return answer;
 }
+//3 limit-40초과인 사람 미리 ㅈ제거하는 부분 삭제하고 for문에서 j가 계속 맨뒤부터 내려오는 것을 j가 합쳐진 부분부터 내려오도록 설정하였더니 효율성 통과 가능해짐
+function solution(people, limit) {
+    var answer = 0;
+    people.sort((a,b)=>a-b);           //정렬을 해두지 않아  실패했었음
+
+     var j=people.length-1
+    for(var i=0;i<people.length-1;i++){
+        for( j=j;j>i;j--){//j=people.length-1;j>i;j--, j=0;j<people.length-i-1;j++
+            if(limit>=(people[i]+people[j])&&people[i]!=="0"&&people[j]!=="0"){
+                answer++;                                //2명이 하나의 보트 타므로 보트 1만큼 증가
+                people.splice(i,1,"0");
+                people.splice(j,1,"0");//배열에서 같이타는 사람들 제거=>splice를 하게 되면 배열이 삭제된다. 그러나 i,j는 삭제되지 않은 배열을 기준으로 증가하기 때문에 비교가 제대로 이루어지지않아 오류가 발생하게 된다. 다른 함수를 사용하던지, for문을 다른 것으로 고쳐 사용해야 된다.
+                break;
+            }
+        }
+    }
+    people=people.filter((element) => element !== "0");
+    answer+=people.length;  // 배열에 남은 같이 탈 수 없는 사람들 수만큼 보트 추가
+    
+    return answer;
+}
