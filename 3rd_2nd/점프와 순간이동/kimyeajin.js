d82f476fd337d8b2460c0707e04be13f94f6a7c0
kimyeajin216099 <86289318+kimyeajin216099@users.noreply.github.com>
2021-07-22 17:48:20 +0900

    Create kimyeajin.js

---
+++ 3rd_2nd/점프와 순간이동/kimyeajin.js
@@ -0,0 +1,16 @@
+/*function solution(n)
+{
+    var ans = 0;
+    var result;
+    while(n!=0){
+        if(n%2===0) n = parseInt(n/2);
+         //console.log(n/2);
+        else{
+            n = n-1; ans++;
+        }
+    }
+    
+    
+    
+    return ans;
+}*/
