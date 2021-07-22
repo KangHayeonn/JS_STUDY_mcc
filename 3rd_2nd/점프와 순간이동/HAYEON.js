3bbde643f7362e31c22c4ec9f47f81a156f91ba4
KangHayeonn <78534650+KangHayeonn@users.noreply.github.com>
2021-07-22 19:29:13 +0900

    HAYEON : 점프와 순간이동
    동적계획법 문제 (DP)
    5 : count += 1 , 5-1
    4 : 4/2
    2 : 2/2
    1 : count += 1, 1-1
    0 : 끝

--- 3rd_2nd/점프와 순간이동/HAYEON.js
+++ 3rd_2nd/점프와 순간이동/HAYEON.js
@@ -1,1 +1,17 @@
-// 작성전
+// 3주차 2차시
+// 점프와 순간이동
+
+function solution(n)
+{
+    var ans = 0;
+    
+    do{
+        if (n%2==0) n = n/2;
+        else {
+            n = n-1;
+            ans++;
+        }
+    } while(n > 0);
+
+    return ans;
+}
