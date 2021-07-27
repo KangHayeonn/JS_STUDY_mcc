c4fa8b9fc010dd05475e6a75d250b23273836eb4
kimyeajin216099 <86289318+kimyeajin216099@users.noreply.github.com>
2021-07-27 13:49:45 +0900

    Create kimyeajin.js

---
+++ 1st_1st/가장 큰 수/kimyeajin.js
@@ -0,0 +1,19 @@
+function solution(numbers) {
+    var i=0,j=0;
+    var answer= new Array();
+    var result= new Array();
+    
+    numbers.sort();
+ 
+      while(i!==numbers.length){
+         result.push(numbers[numbers.length-i-1]);
+         i++;
+        }
+
+    i=0;
+       while(i!==numbers.length){
+          answer+=String(result[i]);
+          i++;
+        }
+    return answer;
+}
