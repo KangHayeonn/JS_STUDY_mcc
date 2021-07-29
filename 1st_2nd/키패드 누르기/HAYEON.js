de35d2aeb18cf9085de19a02b256a4e75fd4d786
KangHayeonn <78534650+KangHayeonn@users.noreply.github.com>
2021-07-29 16:19:07 +0900

    HAYEON : 키패드 누르기
    성공

--- 1st_2nd/키패드 누르기/HAYEON.js
+++ 1st_2nd/키패드 누르기/HAYEON.js
@@ -1,6 +1,7 @@
 // 1주차 2차시 
 // 키패드 누르기
 
+/*
 function solution(numbers, hand) {
     var answer = '';
     var left_hand, right_hand ;
@@ -176,3 +177,119 @@ function solution(numbers, hand) {
     }
     return answer;
 }
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// 2021-07-29 변경
+
+// ver1 : 실패
+// test 13~20 실패
+// 피타고라스 정리를 이용해서 루트로 길이를 구함 (두점 사이의 거리 공식)
+
+function solution(numbers, hand){
+    var answer = '';
+    const keypad = [[1,0],[0,3],[1,3],[2,3],[0,2],[1,2],[2,2],[0,1],[1,1],[2,1]]
+    var right_hand = [2,0];
+    var left_hand = [0,0];
+
+    for(let i of numbers){
+        if(i===1 || i===4 || i===7){
+            left_hand=keypad[i];
+            answer += "L";
+        }
+        else if(i===3 || i===6 || i===9){
+            right_hand=keypad[i];
+            answer += "R";
+        }
+        
+        else if(i===2 || i===5 || i===8 || i===0){ // x2 : keypad[i][0] , y2 : keypad[i][1]
+            //let distance = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+            
+            // 피타고라스 정리
+            let distance_r = Math.sqrt(Math.pow(keypad[i][0]-right_hand[0],2)+Math.pow(keypad[i][1]-right_hand[1],2)); 
+            // x1 : right_hand[0] , y1: right_hand[1]
+
+            let distance_l = Math.sqrt(Math.pow(keypad[i][0]-left_hand[0],2)+Math.pow(keypad[i][1]-left_hand[1],2));
+            // x1 : left_hand[0] , y1: left_hand[1]
+
+            if(distance_r>distance_l){
+                left_hand = keypad[i]; 
+                answer += "L";   
+            }
+            else if(distance_r<distance_l){
+                right_hand = keypad[i];
+                answer += "R";
+            }
+            else if(distance_r === distance_r){
+                if(hand=="right"){
+                    right_hand = keypad[i];
+                    answer += "R";
+                }
+                else if(hand=="left"){
+                    left_hand = keypad[i];
+                    answer += "L";
+                }
+            }
+            
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],"right"));
+
+// ver2 : 성공
+// x 끼리의 직선거리 + y 끼리의 직선거리 로 구해줌
+
+function solution(numbers, hand){
+    var answer = '';
+    const keypad = [[1,0],[0,3],[1,3],[2,3],[0,2],[1,2],[2,2],[0,1],[1,1],[2,1]]
+    var right_hand = [2,0];
+    var left_hand = [0,0];
+
+    for(let i of numbers){
+        if(i===1 || i===4 || i===7){
+            left_hand=keypad[i];
+            answer += "L";
+        }
+        else if(i===3 || i===6 || i===9){
+            right_hand=keypad[i];
+            answer += "R";
+        }
+        
+        else if(i===2 || i===5 || i===8 || i===0){ // x2 : keypad[i][0] , y2 : keypad[i][1]
+            //let distance = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+            
+            // 각각 좌표의 직선상의 거리를 이용해서 distance 구하기
+            let distance_r = Math.abs(keypad[i][0]-right_hand[0]) + Math.abs(keypad[i][1]-right_hand[1]); 
+
+            let distance_l = Math.abs(keypad[i][0]-left_hand[0]) + Math.abs(keypad[i][1]-left_hand[1]);
+
+            if(distance_r>distance_l){
+                left_hand = keypad[i]; 
+                answer += "L";   
+            }
+            else if(distance_r<distance_l){
+                right_hand = keypad[i];
+                answer += "R";
+            }
+            else if(distance_r === distance_r){
+                if(hand=="right"){
+                    right_hand = keypad[i];
+                    answer += "R";
+                }
+                else if(hand=="left"){
+                    left_hand = keypad[i];
+                    answer += "L";
+                }
+            }
+            
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0],"right"));
